9b9e374f48d36ce4aee7e353a6a5b996febb8785
Arnaud <arnaudpadula5@gmail.com>
2019-12-09 17:10:37 +0100

    CRUD

---
+++ src/Form/DemandeType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Demande;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DemandeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date_debut')
+            ->add('date_fin')
+            ->add('nom')
+            ->add('prenom')
+            ->add('email')
+            ->add('telephone')
+            ->add('adresse')
+            ->add('ville')
+            ->add('cp')
+            ->add('created_at')
+            ->add('updated_at')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Demande::class,
+        ]);
+    }
+}
