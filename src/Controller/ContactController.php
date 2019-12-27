5946fd2b23a8f4d894dd812ca33a654a48ffc6fc
Sesmose <arnaudpadula5@gmail.com>
2019-12-27 09:23:24 +0100

    toujours du front

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,39 +1,24 @@
 <?php
 namespace App\Controller;
 
-use App\Form\ContactType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
-class ContactController extends AbstractController {
-	/**
-	 * @Route("/contact",name="contact")
-	 */
-	public function index(request $request, \Swift_Mailer $mailer) {
-		$form = $this->createForm(ContactType::class);
-		$form->handleRequest($request);
+class ContactController
+{
 
-		if ($form->isSubmitted() && $form->isValid()) {
-			$contactFormData = $form->getData();
+    /**
+     * @var Environment
+     */
+    private $twig;
 
-			$message = (new \Swift_Message('Nouvelle demande de réservation de ' . ' ' . $contactFormData['Nom'] . ' ' . $contactFormData['Prenom'] . ' Plus de détails..'))
-				->setFrom($contactFormData['Email'])
-				->setTo('villadaisycorse@gmail.com')
-				->setBody(
-					$contactFormData['Message'] . '
-            mail de l\'expéditeur: ' . $contactFormData['Email'] . ' et le reste ici.',
-					'text/plain'
-				);
-			$mailer->send($message);
-			//$this->addFlash('success', 'Une nouvelle demande d\'inscription a été envoyé');
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
 
-			return $this->redirectToRoute('contact');
-		}
-
-		return $this->render('contact/index.html.twig', [
-			'email_form' => $form->createView(),
-		]);
-	}
-
+    public function index(): Response
+    {
+        return new Response($this->twig->render('pages/contact.html.twig'));
+    }
 }
